e069be601c93989cd1e1d397bc81b2ae6ac61953
StephenSant <42423953+StephenSant@users.noreply.github.com>
2018-09-12 22:59:29 +1000

    Adding shoot

---
+++ Bomb Game/Assets/Scripts/Bomb.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    public float shootForce;
+    public Rigidbody rigidbody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.AddForce(Vector3.forward * shootForce, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
--- Bomb Game/Assets/Scripts/PlayerController.cs
+++ Bomb Game/Assets/Scripts/PlayerController.cs
@@ -9,23 +9,44 @@ public class PlayerController : MonoBehaviour
 
     public CharacterController player;
     public GameObject cam;
+    public GameObject bomb;
+
+    private Vector3 spawnpoint;
 
 	// Use this for initialization
 	void Start ()
     {
         player = GetComponent<CharacterController>();
         cam = GameObject.Find("Main Camera");
+        spawnpoint = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
+        //Player movement
         Vector3 moveDir = (Input.GetAxisRaw("Horizontal") * transform.right + Input.GetAxisRaw("Vertical") * transform.forward).normalized * moveSpeed;
         player.Move(new Vector3 (moveDir.x, player.velocity.y, moveDir.z)* Time.deltaTime);
     
+        //Camera movement
         cam.transform.Rotate(new Vector3(-Input.GetAxis("Mouse Y"), 0, 0));
         transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0));
 
-        if (!player.isGrounded) { player.SimpleMove( Vector3.down * 0.918f); }
+        //Gravity
+        if (!player.isGrounded)
+        {
+            player.SimpleMove( Vector3.down * 0.918f);
+        }
+
+        //Out of bounds
+        if (transform.position.y <= 5)
+        {
+            transform.position = spawnpoint;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bomb,transform.position,cam.transform.rotation,null);
+        }
     }
 }
